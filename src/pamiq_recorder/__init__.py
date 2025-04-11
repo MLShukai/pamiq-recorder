c941a885d82ae8f088704f243e887e7ee2f2360e
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-04-11 13:48:48 +0900

    feat(video): add VideoRecorder

--- src/pamiq_recorder/__init__.py
+++ src/pamiq_recorder/__init__.py
@@ -1,7 +1,8 @@
 from importlib import metadata
 
 from .base import Recorder
+from .video import VideoRecorder
 
 __version__ = metadata.version(__name__.replace("_", "-"))
 
-__all__ = ["Recorder"]
+__all__ = ["Recorder", "VideoRecorder"]
